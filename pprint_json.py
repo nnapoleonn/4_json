a0f4abfbfd38eca815b0bb17f4038556514b5c6c
nnapoleonn <nnapoleonn@mail.ru>
2018-03-03 12:16:50 +0300

    Finished script.

--- pprint_json.py
+++ pprint_json.py
@@ -4,13 +4,19 @@ import sys
 
 
 def load_data(filepath):
-    with open(filepath) as from_json:
-        return json.loads(from_json.read())
+    with open(filepath) as data_from_json:
+        return json.loads(data_from_json.read())
 
 
-def pretty_print_json(new_data):
-    print(json.dumps(new_data, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(data_from_json):
+    print(json.dumps(data_from_json, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data(sys.argv[1]))
+    try:
+        data_in_file = load_data(sys.argv[1])
+        pretty_print_json(data_in_file)
+    except IndexError:
+        print('Enter path to file.')
+    except FileNotFoundError:
+        print('File not found.')
